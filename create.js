11dde4e421da946ecf584e1b677957ffa1e69547
Garrick Sewsankar <gsewsankar@ufl.edu>
2020-05-06 18:55:36 -0400

    Create TableName for Test

--- create.js
+++ create.js
@@ -5,7 +5,7 @@ import dynamoDb from "./libs/dynamodb-lib";
 export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
   const params = {
-    TableName: process.env.tableName,
+    TableName: "notes",
     // 'Item' contains the attributes of the item to be created
     // - 'userId': user identities are federated through the
     //             Cognito Identity Pool, we will use the identity id
