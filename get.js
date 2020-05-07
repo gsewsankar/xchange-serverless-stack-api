876081d87042271e0d2f7fe20198cfb965162fc9
Garrick Sewsankar <gsewsankar@ufl.edu>
2020-05-06 21:56:40 -0400

    tablename sets all

--- get.js
+++ get.js
@@ -3,7 +3,7 @@ import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   const params = {
-    TableName: process.env.tableName,
+    TableName: "notes",
     // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
