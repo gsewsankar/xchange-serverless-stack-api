202f2609bda2853a482adba836b6247932dc3696
Garrick Sewsankar <gsewsankar@ufl.edu>
2020-05-07 18:08:54 -0400

    reupdated tablename to dev - post

--- delete.js
+++ delete.js
@@ -3,7 +3,7 @@ import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   const params = {
-    TableName: "notes",
+    TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be removed
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
